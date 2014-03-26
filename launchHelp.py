52b7d799a272f9c1f717d83180ef89f4b600c019
linkarys <linkarys@gmail.com>
2014-03-26 17:47:06 +0800

    cool

--- launchHelp.py
+++ launchHelp.py
@@ -6,8 +6,33 @@ import json
 import os, re
 from html.parser import HTMLParser
 
-class sys:
+class fn:
     settings = sublime.load_settings("CFDocsLauncher.sublime-settings")
+
+    # match pattern like google lan:en key:python other:ot
+    full_arg = re.compile('^[\w]+([^\S\n]+\w+[^\S\n]*:[^\S\n]*\w+)+[^\S\n]*$')
+    # match pattern like google lan:en key:python other:ot
+    short_arg = re.compile('^[\w]+([^\S\n]+\w+)+[^\S\n]*$')
+    # match pattern like php
+    single_word = re.compile('^[^\S\n][\w]+[^\S\n]*$')
+
+    baseurl = 'https://www.google.com/search?q='
+
+
+    @staticmethod
+    def get_url(input):
+        m = fn.full_arg.search(input)
+        if m is None:
+            m = fn.short_arg.match(input)
+            if m is None:
+                m = fn.single_word.match(input)
+
+        if m is not None:
+            return (m.groups())
+
+    @staticmethod
+    def get(attr):
+        return fn.settings.get(attr, fn.baseurl)
 
 def get_syntax(view):
     scope = view.scope_name(view.sel()[0].end())
@@ -35,13 +60,13 @@ class LaunchCfHelpCommand(sublime_plugin.TextCommand):
 def get_lan_settings(self):
     syntax = get_syntax(self.view)
 
-    return sys.settings.get(syntax)
+    return fn.settings.get(syntax)
 
 def build_load_url(self):
     keyword = get_word(self.view)
     syntax = get_syntax(self.view)
 
-    lan_settings = sys.settings.get(syntax)
+    lan_settings = fn.settings.get(syntax)
 
     if 'doc_url' in lan_settings:
         url = lan_settings['doc_url']
@@ -56,7 +81,7 @@ def build_search_url(self):
     keyword = get_word(self.view)
     syntax = get_syntax(self.view)
 
-    lan_settings = sys.settings.get(syntax)
+    lan_settings = fn.settings.get(syntax)
 
     if 'search_url' in lan_settings:
         url = lan_settings['search_url']
@@ -73,20 +98,33 @@ class SearchCfDocsCommand(sublime_plugin.TextCommand):
 
         webbrowser.open(url)
 
-class SearchInputCommand(sublime_plugin.TextCommand):
+class InputSearchCommand(sublime_plugin.TextCommand):
 
+    def on_done(self, word):
+        print (word)
     def run(self, edit):
+        url = build_search_url(self)
 
-        self.window.show_input_panel('Search Stack Overflow for', '',
-            self.on_done, self.on_change, self.on_cancel)
-        def on_done(self, input):
-            SearchFor(input)
+        webbrowser.open(url)
 
-        def on_change(self, input):
-            pass
+class SearchInputCommand(sublime_plugin.WindowCommand):
 
-        def on_cancel(self):
-            pass
+    def on_done(self, input):
+       url = fn.get_url(input)
+       # print (url)
+       webbrowser.open(url)
+
+    def on_change(self, input):
+        pass
+
+    def on_cancel(self):
+        pass
+
+    def run(self):
+
+        # self.window.show_quick_panel(['xinju', 'yang', 'jiujiu'], self.on_done)
+        self.window.show_input_panel('Search ju for', '', self.on_done, self.on_change, self.on_cancel)
+
 
         # url = "http://www.baidu.com"
         # parser = MyHTMLParser()
