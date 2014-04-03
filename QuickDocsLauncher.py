ad6d34f8c5f7f8e1dcce7f12da15acf21aafcb1b
linkarys <linkarys@gmail.com>
2014-04-03 10:49:16 +0800

    tweak

--- QuickDocsLauncher.py
+++ QuickDocsLauncher.py
@@ -6,54 +6,45 @@ import json
 import os, re
 from html.parser import HTMLParser
 
-class fn:
-    # match pattern like google lan:en key:python other:ot
-    full_arg = re.compile(r'^[\w]+([^\S\n]+\w+[^\S\n]*:[^\S\n]*\w+)+[^\S\n]*$')
-    # match pattern like google lan:en key:python other:ot
-    short_arg = re.compile(r'^[\w]+([^\S\n]+\w+)+[^\S\n]*$')
-    # match pattern like php
-    single_word = re.compile(r'^[^\S\n][\w]+[^\S\n]*$')
+def get_url(input):
 
-    @staticmethod
-    def get_url(input):
+    settings = sublime.load_settings("QuickDocsLauncher.sublime-settings")
+    s_patterns = settings.get('search_patterns')
+    arg_pair = re.compile(r'^(\w+):([\w\d.-]*)$')
 
-        settings = sublime.load_settings("QuickDocsLauncher.sublime-settings")
-        s_patterns = settings.get('search_patterns')
-        arg_pair = re.compile(r'^(\w+):([\w\d.-]*)$')
+    words = input.split()
+    syntax = words.pop(0)
 
-        words = input.split()
-        syntax = words.pop(0)
+    def repl_fun(key, val):
+        def repl(matchobj):
+            if matchobj.group(1) == key:
+                return str(val)
+            return matchobj.group(0)
+        return repl
 
-        def repl_fun(key, val):
-            def repl(matchobj):
-                if matchobj.group(1) == key:
-                    return str(val)
-                return matchobj.group(0)
-            return repl
+    def fillin_default(matchobj):
+        return matchobj.group(2)
 
-        def fillin_default(matchobj):
-            return matchobj.group(2)
+    try:
+        s_pattern = s_patterns[syntax]['pattern']
+        reg_repl = re.compile(r'\$\{\s*([\w\d+]+)\s*:([^\}]*)\}')
+        keyword = []
+        for word in words:
+            match = arg_pair.match(word)
+            if match:
+                key = match.group(1)
+                val = match.group(2)
 
-        try:
-            s_pattern = s_patterns[syntax]['pattern']
-            reg_repl = re.compile(r'\$\{\s*([\w\d+]+)\s*:([^\}]*)\}')
-            keyword = []
-            for word in words:
-                match = arg_pair.match(word)
-                if match:
-                    key = match.group(1)
-                    val = match.group(2)
+                repl = repl_fun(key, val)
+                s_pattern = reg_repl.sub(repl, s_pattern)
+            else:
+                keyword.append(word)
 
-                    repl = repl_fun(key, val)
-                    s_pattern = reg_repl.sub(repl, s_pattern)
-                else:
-                    keyword.append(word)
+        s_pattern = reg_repl.sub(fillin_default, s_pattern)
+        return s_pattern + ' '.join(keyword)
 
-            s_pattern = reg_repl.sub(fillin_default, s_pattern)
-            return s_pattern + ' '.join(keyword)
-
-        except:
-            return settings.get('default', 'https://www.google.com/search?q=') + input
+    except:
+        return settings.get('default', 'https://www.google.com/search?q=') + input
 
 def get_syntax(view):
     scope = view.scope_name(view.sel()[0].end())
@@ -99,7 +90,7 @@ class SearchDocsCommand(sublime_plugin.TextCommand):
 class SearchInputCommand(sublime_plugin.WindowCommand):
 
     def on_done(self, input):
-       webbrowser.open(fn.get_url(input))
+       webbrowser.open(get_url(input))
 
     def on_change(self, input):
         pass
